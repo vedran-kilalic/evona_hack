e5f18874f06ced67d43e29832f9f148b27293c2f
Feđa Pandžić <fedjap@comtrade.com>
2022-10-22 12:05:20 +0200

    restart

---
+++ src/main/java/com/team3/evona/Hackathon2022Application.java
@@ -0,0 +1,13 @@
+package com.team3.evona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/evona/Hackathon2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.evona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
